03283a7577023459a0b1736aee2de2985c9dfe22
brunalvess37 <brunalvess@hotmail.com>
2026-01-17 14:19:12 -0300

    Update postos.js

--- functions/api/postos.js
+++ functions/api/postos.js
@@ -1,1 +1,21 @@
+export async function onRequestGet({ env }) {
+  try {
+    const data = await env.POSTOS_KV.get("postos", "json");
 
+    if (!data) {
+      return new Response("[]", {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" }
+    });
+
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ error: "Erro ao ler dados do KV", detail: err.message }),
+      { status: 500 }
+    );
+  }
+}
