bde4e931d627c50393bff56ae46b8a5afe013593
brunalvess37 <brunalvess@hotmail.com>
2026-01-17 15:04:04 -0300

    Update upload-postos.js

--- functions/api/upload-postos.js
+++ functions/api/upload-postos.js
@@ -1,1 +1,18 @@
+export async function onRequestPost({ request, env }) {
+  try {
+    const body = await request.json();
 
+    if (!body.data || !Array.isArray(body.data)) {
+      return new Response("Dados inválidos", { status: 400 });
+    }
+
+    await env.POSTOS_KV.put(
+      "POSTOS",
+      JSON.stringify(body.data)
+    );
+
+    return new Response("✔ Dados salvos no KV com sucesso");
+  } catch (err) {
+    return new Response("Erro ao salvar dados", { status: 500 });
+  }
+}
