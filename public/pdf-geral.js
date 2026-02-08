882de4194df19cc86b631406db834fe0bfb3f13f
brunalvess37 <brunalvess@hotmail.com>
2026-02-07 22:14:12 -0300

    Update pdf-geral.js

--- public/pdf-geral.js
+++ public/pdf-geral.js
@@ -130,7 +130,7 @@ async function gerarPDFGeral(filtros) {
     // ===== BLOCO DO POSTO =====
 conteudo.push({
   margin: [0, 0, 0, 14],
-  pageBreak: 'avoid',   // 👈 sem quebra de página no bloco
+  unbreakable: true,   // 👈 sem quebra de página no bloco
   stack: [
 
     // Nome do posto
